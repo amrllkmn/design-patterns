244b8dd2dc4eb325506b194c078c1a74dd94781c
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2022-10-16 22:48:59 +0800

    Add HeatIndexDisplay

---
+++ observer/src/classes/HeatIndexDisplay.ts
@@ -0,0 +1,72 @@
+import DisplayElement from '../interfaces/display';
+import Observer from '../interfaces/observer';
+import WeatherData from './WeatherData';
+
+export default class HeatIndexDisplay implements Observer, DisplayElement {
+  private temperature: number;
+
+  private humidity: number;
+
+  private weatherData: WeatherData;
+
+  constructor(weatherData: WeatherData) {
+    this.weatherData = weatherData;
+    this.temperature = 0;
+    this.humidity = 0;
+    weatherData.registerObserver(this);
+  }
+
+  private computeHeatIndex(): number {
+    const index =
+      16.923 +
+      0.185212 * this.temperature +
+      5.37941 * this.humidity -
+      0.100254 * this.temperature * this.humidity +
+      0.00941695 * (this.temperature * this.temperature) +
+      0.00728898 * (this.humidity * this.humidity) +
+      0.000345372 * (this.temperature * this.temperature * this.humidity) -
+      0.000814971 * (this.temperature * this.humidity * this.humidity) +
+      0.0000102102 *
+        (this.temperature * this.temperature * this.humidity * this.humidity) -
+      0.000038646 * (this.temperature * this.temperature * this.temperature) +
+      0.0000291583 * (this.humidity * this.humidity * this.humidity) +
+      0.00000142721 *
+        (this.temperature *
+          this.temperature *
+          this.temperature *
+          this.humidity) +
+      0.000000197483 *
+        (this.temperature * this.humidity * this.humidity * this.humidity) -
+      0.0000000218429 *
+        (this.temperature *
+          this.temperature *
+          this.temperature *
+          this.humidity *
+          this.humidity) +
+      0.000000000843296 *
+        (this.temperature *
+          this.temperature *
+          this.humidity *
+          this.humidity *
+          this.humidity) -
+      0.0000000000481975 *
+        (this.temperature *
+          this.temperature *
+          this.temperature *
+          this.humidity *
+          this.humidity *
+          this.humidity);
+    return index;
+  }
+
+  display(): void {
+    const index = this.computeHeatIndex();
+    console.log(`Heat index is ${index}`);
+  }
+
+  update(temp: number, humidity: number, pressure: number): void {
+    this.temperature = temp;
+    this.humidity = humidity;
+    this.display();
+  }
+}
--- observer/src/index.ts
+++ observer/src/index.ts
@@ -1,4 +1,5 @@
 import CurrentConditionsDisplay from './classes/CurrentConditionsDisplay';
+import HeatIndexDisplay from './classes/HeatIndexDisplay';
 import StatisticsDisplay from './classes/StatisticsDisplay';
 import WeatherData from './classes/WeatherData';
 
@@ -6,6 +7,7 @@ function main() {
   const weatherData = new WeatherData();
   const currentDisplay = new CurrentConditionsDisplay(weatherData);
   const statisticsDisplay = new StatisticsDisplay(weatherData);
+  const heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
   weatherData.setMeasurement(80, 65, 30.4);
   weatherData.setMeasurement(82, 70, 29.2);
